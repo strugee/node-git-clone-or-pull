c9ffa4567e40eae8b3e4a906cf6a4a15acf88763
Alex Jordan <alex@strugee.net>
2016-10-28 22:19:38 -0700

    Correct subprocess backend test suite name

--- test/subprocess-backend-basic.js
+++ test/subprocess-backend-basic.js
@@ -3,7 +3,7 @@
 var vows = require('vows'),
     backendTest = require('./lib/backend-test.js');
 
-var suite = vows.describe('NodeGit backend');
+var suite = vows.describe('Subprocess backend');
 
 suite.addBatch(backendTest({implementation: 'subprocess'}));
 
