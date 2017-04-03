cf0ecb12da2081599f40691a5f329583b8cde389
Alex Jordan <alex@strugee.net>
2017-04-03 15:08:47 -0400

    Add test cases for the branch logic

---
+++ test/lib/branch-test.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var assert = require('assert'),
+    path = require('path'),
+    fs = require('fs'),
+    assign = require('lodash.assign'),
+    smartSpawn = require('smart-spawn'),
+    repoPath = path.join(process.cwd(), 'strugee.github.com');
+
+module.exports = function(options) {
+	var cloneOrPull;
+
+	return {
+		'When we require the module': {
+			topic: function() {
+				/* jshint boss: true */
+				return cloneOrPull = require('../../index.js');
+			},
+			'it works': function(cloneOrPull) {
+				assert.isFunction(cloneOrPull);
+			},
+			'and we clone something and specify the branch': {
+				topic: function(cloneOrPull) {
+					cloneOrPull('git://github.com/strugee/strugee.github.com.git', assign({}, options, {branch: 'src', path: repoPath}), this.callback);
+				},
+				'the directory exists': function() {
+					fs.access(repoPath, function(err) {
+						assert.ifError(err);
+					});
+				},
+				'the src/ directory exists': function() {
+					fs.access(path.join(repoPath, 'src'), function(err) {
+						assert.ifError(err);
+					});
+				}
+			}
+		}
+	};
+};
---
+++ test/subprocess-backend-branch.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var vows = require('vows'),
+    backendTest = require('./lib/branch-test.js');
+
+var suite = vows.describe('Subprocess backend with branch options');
+
+suite.addBatch(backendTest({implementation: 'subprocess'}));
+
+suite['export'](module);
